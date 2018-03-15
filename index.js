c859b822a4abf84fe0691bd03847d2c9b31032f3
Lisa Shaughnessy <lisavshaughnessy@gmail.com>
2018-03-15 06:06:10 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,12 @@
 function happyHolidays() {
   return ("Happy holidays!")
 }
-
+function happyHolidaysTo () {
+  return (`Happy holidays, ${name}!`)
+}
+function happyHolidayTo () {
+  return (`Happy ${holiday}, ${name}!`)
+}
+function holidayCountdown () {
+  return (`It's ${days} days until ${holiday}!`)
+}
